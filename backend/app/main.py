ade46f8f2598c428f6055f466c546e833b4b54ce
Adamiec <wojciechadamiec99@gmail.com>
2020-10-16 20:29:00 +0200

    added sqlalchemy to requirements

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,18 @@
 """
 
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db/ctdb"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 app = FastAPI()
 
