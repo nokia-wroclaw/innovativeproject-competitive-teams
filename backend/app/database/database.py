9e02052aa9756c38b4623602a1e432d07b66021c
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2020-10-18 17:07:32 +0200

    updated backend to get db url from environment

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from os import getenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db/ctdb"
+SQLALCHEMY_DATABASE_URL = getenv("DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL)
+# for dev environment with docker-compose
+if SQLALCHEMY_DATABASE_URL == "":
+    SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db/ctdb"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
