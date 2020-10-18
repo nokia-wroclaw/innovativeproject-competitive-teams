79cd4687e6b2b4f0b2d49b521886abf89775d9c0
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2020-10-18 22:20:40 +0200

    moved dev db configuration to docker-compose

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -5,10 +5,6 @@ from os import getenv
 
 SQLALCHEMY_DATABASE_URL = getenv("DATABASE_URL")
 
-# for dev environment with docker-compose
-if SQLALCHEMY_DATABASE_URL is None:
-    SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db/ctdb"
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
