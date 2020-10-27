f5b443d2d8ed4e2ecb84ce9dffc16125ddab29a8
bartektroszka <bartek99onet.com.pl@onet.pl>
2020-10-27 23:25:09 +0100

    environment for firebase

--- frontend/src/Auth.js
+++ frontend/src/Auth.js
@@ -1,37 +1,3 @@
-/*import  { useEffect, useState } from "react";
-import * as React from "react";
-import app from "./base";
-
-
-
-
-export const AuthContext = React.createContext(null);
-
-export const AuthProvider: React.FC<{}> = ({ children }) => {
-const [currentUser, setCurrentUser] = useState(null);
-const [pending, setPending] = useState(true);
-
-useEffect(() => {
-app.auth().onAuthStateChanged((user) => {
-setCurrentUser(user)
-setPending(false)
-});
-}, []);
-
-if(pending){
-return <>Loading...</>
-}
-
-return (
-<AuthContext.Provider
-value={{
-currentUser
-}}
->
-{children}
-</AuthContext.Provider>
-);
-};*/
 import React, { useEffect, useState } from "react";
 import app from "./base";
 
