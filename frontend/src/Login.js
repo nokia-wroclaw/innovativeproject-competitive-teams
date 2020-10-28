497c1bd570d2c1c4269b4ebbbded843712153cef
bartektroszka <bartek99onet.com.pl@onet.pl>
2020-10-28 16:15:42 +0100

    corrected formatting

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,51 +3,49 @@ import * as React from "react";
 import { withRouter, Redirect } from "react-router";
 import app from "./base";
 import { AuthContext } from "./Auth";
-import firebase from 'firebase/app';
-import { signInWithGoogle } from './base';
+import firebase from "firebase/app";
+import { signInWithGoogle } from "./base";
 
 const Login = ({ history }) => {
-const handleLogin = useCallback(
-async event => {
-event.preventDefault();
-const { email, password } = event.target.elements;
-try {
-await app
-.auth()
-.signInWithEmailAndPassword(email.value, password.value);
-history.push("/");
-} catch (error) {
-alert(error);
-}
-},
-[history]
-);
+  const handleLogin = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await app
+          .auth()
+          .signInWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
 
+  const { currentUser } = useContext(AuthContext);
 
-  
-const { currentUser } = useContext(AuthContext);
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
 
-if (currentUser) {
-return <Redirect to="/" />;
-}
-
-return (
-<div>
-<h1>Log in</h1>
-<form onSubmit={handleLogin}>
-<label>
-Email
-<input name="email" type="email" placeholder="Email" />
-</label>
-<label>
-Password
-<input name="password" type="password" placeholder="Password" />
-</label>
-<button type="submit">Log in</button>
-<button onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</button>
-</form>
-</div>
-);
+  return (
+    <div>
+      <h1>Log in</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email
+          <input name="email" type="email" placeholder="Email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" placeholder="Password" />
+        </label>
+        <button type="submit">Log in</button>
+        <button onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</button>
+      </form>
+    </div>
+  );
 };
 
 export default withRouter(Login);
--- frontend/src/base.js
+++ frontend/src/base.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebase from "firebase/app";
+import "firebase/database";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
@@ -8,16 +8,13 @@ const app = firebase.initializeApp({
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
 });
-
 
 export const auth = firebase.auth();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default app;
-
-
