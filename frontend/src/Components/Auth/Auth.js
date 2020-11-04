ccf29bb0665b836f26afe7e40b07879fc4512e70
bartektroszka <bartek99onet.com.pl@onet.pl>
2020-11-04 12:27:35 +0100

    format

--- frontend/src/Components/Auth/Auth.js
+++ frontend/src/Components/Auth/Auth.js
@@ -4,9 +4,7 @@ import app from "../Base/base";
 export const AuthContext = React.createContext();
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
-
   const [pending, setPending] = useState(true);
-
   useEffect(() => {
     app.auth().onAuthStateChanged((user) => {
       setCurrentUser(user);
