6b933fa5ecc83a570dd9874c237f96a33231bf11
bartektroszka <bartek99onet.com.pl@onet.pl>
2020-11-05 21:59:10 +0100

    SignUp component

--- frontend/src/Components/SignUp/SignUp.js
+++ frontend/src/Components/SignUp/SignUp.js
@@ -2,39 +2,77 @@ import { useCallback } from "react";
 import React from "react";
 import { withRouter } from "react-router";
 import app from "../Base/base";
+import { Form, Input, Button, Checkbox } from "antd";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
 
 const SignUp = ({ history }) => {
-  const handleSignUp = useCallback(
-    async (event) => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
-      try {
-        await app
-          .auth()
-          .createUserWithEmailAndPassword(email.value, password.value);
-        history.push("/");
-      } catch (error) {
-        alert(error);
-      }
-    },
-    [history]
-  );
+  const onFinish = (values) => {
+    app.auth().createUserWithEmailAndPassword(values.username, values.password);
+    history.push("/");
+  };
 
   return (
-    <div>
-      <h1>Sign up</h1>
-      <form onSubmit={handleSignUp}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+    <Form
+      style={{
+        position: "absolute",
+        left: "45%",
+        top: "30%",
+        transform: "translate(-50%, -50%)",
+      }}
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={console.log("error")}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Sign Up
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
