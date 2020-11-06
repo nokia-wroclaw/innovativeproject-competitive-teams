02a5e7bcb29972921edb0b6948a23e6c57e3a0bd
bartektroszka <bartek99onet.com.pl@onet.pl>
2020-11-06 13:34:10 +0100

    signup and login views

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -3,24 +3,44 @@ import * as React from "react";
 import { withRouter, Redirect } from "react-router";
 import { AuthContext } from "../Auth/Auth";
 import app, { signInWithGoogle } from "../Base/base";
+import { Space, Card, Row, Form, Input, Button } from "antd";
+import { Api } from "../../Api";
+import { Anchor } from "antd";
 
-const Login = ({ history }) => {
-  const handleLogin = useCallback(
-    async (event) => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
-      try {
-        await app
-          .auth()
-          .signInWithEmailAndPassword(email.value, password.value);
-        history.replace("/dashboard/profile");
-      } catch (error) {
-        alert(error);
-      }
-    },
-    [history]
-  );
+const { Link } = Anchor;
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 15,
+  },
+};
 
+const LogIn = ({ history }) => {
+  const onFinish = (values) => {
+    app.auth().signInWithEmailAndPassword(values.username, values.password);
+    history.replace("/dashboard/profile");
+    ///basic fireb-db connection:
+
+    var user = app.auth().currentUser;
+    var user_uid = user.uid;
+    console.log(user_uid);
+
+    Api.post("/players", {
+      name: user_uid.substr(0, 5),
+      description: user_uid.substr(5),
+      firebase_id: user_uid,
+    })
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  };
   const { currentUser } = useContext(AuthContext);
 
   if (currentUser) {
@@ -28,25 +48,74 @@ const Login = ({ history }) => {
   }
 
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-      <button onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</button>
-      <button onClick={() => history.replace("/signup")}>
-        SIGNUP WITH EMAIL
-      </button>
-    </div>
+    <Row
+      type="flex"
+      justify="middle"
+      align="center"
+      style={{ minHeight: "80vh" }}
+    >
+      <Card
+        title="Sign in"
+        justify="middle"
+        allign="center"
+        style={{ width: 450 }}
+      >
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={console.log("error")}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Space direction="vertical" size={5}>
+              <Space size={5}>
+                <Button type="primary" htmlType="submit">
+                  Sign in
+                </Button>
+                <Button onClick={signInWithGoogle}>Sign in with Google</Button>
+              </Space>
+              <a href="/SignUp">Sign Up</a>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </Row>
   );
 };
 
-export default withRouter(Login);
+export default withRouter(LogIn);
+
+/*
+<Button onClick={() => history.replace("/signup")}>
+        Sign up with email
+      </Button>
+      */
--- frontend/src/Components/SignUp/SignUp.js
+++ frontend/src/Components/SignUp/SignUp.js
@@ -1,8 +1,8 @@
-import { useCallback } from "react";
 import React from "react";
 import { withRouter } from "react-router";
 import app from "../Base/base";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Card, Row, Form, Input, Button } from "antd";
+import { Api } from "../../Api";
 
 const layout = {
   labelCol: {
@@ -12,6 +12,7 @@ const layout = {
     span: 16,
   },
 };
+
 const tailLayout = {
   wrapperCol: {
     offset: 8,
@@ -23,56 +24,75 @@ const SignUp = ({ history }) => {
   const onFinish = (values) => {
     app.auth().createUserWithEmailAndPassword(values.username, values.password);
     history.push("/");
+    var user = app.auth().currentUser;
+    var user_uid = user.uid;
+    console.log(user_uid);
+
+    Api.post("/players", {
+      name: user_uid.substr(0, 5),
+      description: user_uid.substr(5),
+      firebase_id: user_uid,
+    })
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
   };
 
   return (
-    <Form
-      style={{
-        position: "absolute",
-        left: "45%",
-        top: "30%",
-        transform: "translate(-50%, -50%)",
-      }}
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={console.log("error")}
+    <Row
+      type="flex"
+      justify="middle"
+      align="center"
+      style={{ minHeight: "80vh" }}
     >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your username!",
-          },
-        ]}
+      <Card
+        title="Sign up"
+        justify="middle"
+        allign="center"
+        style={{ width: 450 }}
       >
-        <Input />
-      </Form.Item>
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={console.log("error")}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Sign Up
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit" size="large">
+              Sign up
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </Row>
   );
 };
 
