73cf864d45ae8b5dfe51b022c2cfad893c2710ac
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2020-11-09 20:30:39 +0100

    refactored the Team component

--- frontend/src/Components/Team/index.js
+++ frontend/src/Components/Team/index.js
@@ -1,86 +1,83 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Card, Table } from "antd";
+import { Typography, Card, Table, Spin } from "antd";
 import { useParams } from "react-router-dom";
 import "./index.css";
 
 import { Api } from "../../Api";
 
-const Team = (props) => {
-  const { Title } = Typography;
+const { Title } = Typography;
+const { Column, ColumnGroup } = Table;
+const { Meta } = Card;
+
+const Team = ({ id }) => {
+  // If no id has been passed, check router params
   const { teamid } = useParams();
+  if (id === null || id === undefined) id = teamid;
+
   const [teamdata, setTeamdata] = useState(null);
-  const [render, setRender] = useState(<Title>Team: {teamid}</Title>);
-  const { Column, ColumnGroup } = Table;
-  const { Meta } = Card;
+  const [err, setErr] = useState(null);
 
   // Get team data
   useEffect(() => {
-    if (props.teamdata) setTeamdata(props.teamdata);
-    else if (teamid) {
-      Api.get("/teams/" + teamid)
+    if (id === null || id === undefined) setErr("No team id passed.");
+    else {
+      Api.get("/teams/" + id)
         .then((response) => {
           if (response.status === 200) {
             setTeamdata(response.data);
           }
         })
         .catch((err) => {
-          setRender(
-            <Title>
-              Api request failed for team with id: {teamid}
-              <br />
-              {err.toString()}
-            </Title>
-          );
+          setTeamdata(null);
+          setErr(err.toString());
         });
-    } else {
-      return <Title> No team id/data! </Title>;
     }
-  }, [props.teamdata, teamid]);
+  }, [id]);
 
-  // Parse team data
-  useEffect(() => {
-    console.log(teamdata);
-    if (teamdata) {
-      setRender(
-        <div className="team-info">
-          <Table
-            dataSource={
-              teamdata.captain_id ? [{ captain_id: teamdata.captain_id }] : null
-            }
-            size="small"
-            pagination={false}
-            bordered={true}
-          >
-            <ColumnGroup title="Captain" align="center">
-              <Column title="Player ID" dataIndex="captain_id" key="playerid" />
-            </ColumnGroup>
-          </Table>
-          <Table
-            dataSource={teamdata.players}
-            size="small"
-            pagination={false}
-            bordered={true}
-          >
-            <ColumnGroup title="Players" align="center">
-              <Column title="Player ID" dataIndex="id" key="playerid" />
-              <Column title="Name" dataIndex="name" key="playername" />
-              <Column
-                title="Description"
-                dataIndex="description"
-                key="playerdesc"
-              />
-            </ColumnGroup>
-          </Table>
+  return teamdata ? (
+    <div className="team-info">
+      <Table
+        dataSource={
+          teamdata.captain_id ? [{ captain_id: teamdata.captain_id }] : null
+        }
+        size="small"
+        pagination={false}
+        bordered={true}
+      >
+        <ColumnGroup title="Captain" align="center">
+          <Column title="Player ID" dataIndex="captain_id" key="playerid" />
+        </ColumnGroup>
+      </Table>
+      <Table
+        dataSource={teamdata.players}
+        size="small"
+        pagination={false}
+        bordered={true}
+      >
+        <ColumnGroup title="Players" align="center">
+          <Column title="Player ID" dataIndex="id" key="playerid" />
+          <Column title="Name" dataIndex="name" key="playername" />
+          <Column
+            title="Description"
+            dataIndex="description"
+            key="playerdesc"
+          />
+        </ColumnGroup>
+      </Table>
 
-          <Card>
-            <Meta title="Description" description={teamdata.description} />
-          </Card>
-        </div>
-      );
-    }
-  }, [teamdata, props.arrow]);
-
-  return render;
+      <Card>
+        <Meta title="Description" description={teamdata.description} />
+      </Card>
+    </div>
+  ) : err ? (
+    <Title>
+      Api request failed for team with id: {id}
+      <br />
+      {err}
+    </Title>
+  ) : (
+    <Spin />
+  );
 };
 
 export default Team;
