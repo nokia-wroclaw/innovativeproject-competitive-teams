ded0d843f41ee04a5154d84e0990d65e8f3edcf7
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2020-11-09 20:30:22 +0100

    refactored the Teams component

--- frontend/src/Components/Teams/index.js
+++ frontend/src/Components/Teams/index.js
@@ -1,48 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { Layout, Card, Collapse, Typography } from "antd";
+import { Layout, Card, Collapse, Typography, Spin } from "antd";
 import "./index.css";
 
 import { Api } from "../../Api";
 import Team from "../Team";
 
+const { Content } = Layout;
+const { Panel } = Collapse;
+const { Title } = Typography;
+
 const Teams = () => {
-  const { Content } = Layout;
-  const { Panel } = Collapse;
-  const { Title } = Typography;
   const [teams, setTeams] = useState(null);
-  const [team_panels, setTeam_panels] = useState(null);
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
     Api.get("/teams/")
       .then((result) => {
         setTeams(result.data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        setTeams(null);
+        setErr(err.toString());
+      });
   }, []);
 
-  useEffect(() => {
-    if (teams) {
-      let panels = [];
-      for (const team of teams) {
-        panels.push(
-          <Panel header={"Team " + team.name} key={team.id}>
-            <Team teamdata={team} />
-          </Panel>
-        );
-      }
-      setTeam_panels(panels);
-    }
-  }, [teams]);
-
-  return (
+  return teams ? (
     <Layout style={{ padding: "24px 24px 24px" }}>
       <Content className="site-layout-background">
         <Card>
           <Title> List of teams </Title>
-          <Collapse>{team_panels}</Collapse>
+          <Collapse>
+            {teams.map((team) => (
+              <Panel header={"Team " + team.name} key={team.id}>
+                <Team id={team.id} />
+              </Panel>
+            ))}
+          </Collapse>
         </Card>
       </Content>
     </Layout>
+  ) : err ? (
+    <Title>
+      Api request failed for the list of teams.
+      <br />
+      {err}
+    </Title>
+  ) : (
+    <Spin />
   );
 };
 
