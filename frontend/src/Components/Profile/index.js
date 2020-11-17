39e875e9c7f159b160fca72d47f0318d51614b91
bartektroszka <bartek99onet.com.pl@onet.pl>
2020-11-17 21:12:35 +0100

    small correction

--- frontend/src/Components/Profile/index.js
+++ frontend/src/Components/Profile/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { Typography } from "antd";
-import { AuthContext } from "../Auth/Auth";
 
 const { Title } = Typography;
 const Profile = ({ userid }) => {
-  console.log(React.useContext(AuthContext)["username"]);
-  console.log(React.useContext(AuthContext)["colour"]);
-  console.log(React.useContext(AuthContext)["rank"]);
-  console.log(React.useContext(AuthContext)["description"]);
   return <Title>Profile: {userid}</Title>;
 };
 
