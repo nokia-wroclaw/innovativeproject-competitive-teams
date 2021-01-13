df5405d35aa7843a99f0d1fdca7fa38ae8d6b566
Adamiec <wojciechadamiec99@gmail.com>
2021-01-13 10:40:07 +0100

    Formatted all backend python files with Black

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -3,17 +3,22 @@ from sqlalchemy.orm import relationship
 
 from app.database.database import Base
 
+
 class PlayerTeam(Base):
     __tablename__ = "players_teams"
 
-    player_id = Column('players_id', ForeignKey('players.id'), primary_key=True)
-    team_id = Column('teams_id', ForeignKey('teams.id'), primary_key=True)
+    player_id = Column("players_id", ForeignKey("players.id"), primary_key=True)
+    team_id = Column("teams_id", ForeignKey("teams.id"), primary_key=True)
+
 
 class TournamentTeam(Base):
     __tablename__ = "tournaments_teams"
 
-    tournament_id = Column('tournaments_id', ForeignKey('tournaments.id'), primary_key=True)
-    team_id = Column('teams_id', ForeignKey('teams.id'), primary_key=True)
+    tournament_id = Column(
+        "tournaments_id", ForeignKey("tournaments.id"), primary_key=True
+    )
+    team_id = Column("teams_id", ForeignKey("teams.id"), primary_key=True)
+
 
 class Team(Base):
     __tablename__ = "teams"
@@ -23,11 +28,12 @@ class Team(Base):
     description = Column(String)
     colour = Column(String, index=True)
 
-    captain_id = Column(Integer, ForeignKey('players.id'))
-    captain = relationship("Player", back_populates='captain_teams')
+    captain_id = Column(Integer, ForeignKey("players.id"))
+    captain = relationship("Player", back_populates="captain_teams")
 
     players = relationship("Player", secondary=PlayerTeam.__tablename__)
     tournaments = relationship("Tournament", secondary=TournamentTeam.__tablename__)
+
 
 class Player(Base):
     __tablename__ = "players"
@@ -39,8 +45,9 @@ class Player(Base):
     colour = Column(String, index=True)
     role = Column(String, index=True)
 
-    captain_teams = relationship("Team", back_populates='captain')
+    captain_teams = relationship("Team", back_populates="captain")
     teams = relationship("Team", secondary=PlayerTeam.__tablename__)
+
 
 class Match(Base):
     __tablename__ = "matches"
@@ -49,23 +56,23 @@ class Match(Base):
     name = Column(String)
     description = Column(String)
 
-    team1_id = Column(Integer, ForeignKey('teams.id'))
+    team1_id = Column(Integer, ForeignKey("teams.id"))
     team1 = relationship("Team", uselist=False, foreign_keys=[team1_id])
 
-    team2_id = Column(Integer, ForeignKey('teams.id'))
+    team2_id = Column(Integer, ForeignKey("teams.id"))
     team2 = relationship("Team", uselist=False, foreign_keys=[team2_id])
 
     start_time = Column(String)
     finished = Column(Boolean)
 
-
     tournament_place = Column(Integer)
 
-    tournament_id = Column(Integer, ForeignKey('tournaments.id'))
+    tournament_id = Column(Integer, ForeignKey("tournaments.id"))
     tournament = relationship("Tournament", back_populates="matches")
 
     score1 = Column(Integer)
     score2 = Column(Integer)
+
 
 class Tournament(Base):
     __tablename__ = "tournaments"
@@ -81,4 +88,3 @@ class Tournament(Base):
 
     matches = relationship("Match", back_populates="tournament")
     teams = relationship("Team", secondary=TournamentTeam.__tablename__)
-
