17d754c4d2a1304e131d17005575a05231d162c3
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2021-01-31 13:56:33 +0100

    added default values to MoidyTournamentMatch form

--- frontend/src/Components/Tournament/ModifyTournamentMatch.js
+++ frontend/src/Components/Tournament/ModifyTournamentMatch.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useQueryClient } from "react-query";
 import { Popover, Button, Col, Form, Input, Space, DatePicker } from "antd";
+import moment from "moment";
 import "./index.css";
 import { AuthContext } from "../Auth/Auth";
 import { Notification } from "../Util/Notification";
@@ -65,6 +66,7 @@ const ModifyTournamentMatch = ({
       name="nest-messages"
       onFinish={onFinish}
       validateMessages={validateMessages}
+      initialValues={{ name: name, time: moment(time) }}
     >
       <Form.Item name="name" label={`Match name`} rules={[{ required: true }]}>
         <Input />
