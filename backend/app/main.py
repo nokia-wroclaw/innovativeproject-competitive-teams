e324d4103ef1f0cb77447647f11be7786d7af0ee
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2021-02-03 12:04:13 +0100

    allow moderator to modify team details

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,7 +40,7 @@ def create_team(
     firebase_id: str = Header(None),
     db: Session = Depends(get_db),
 ):
-    clearance = "admin"
+    clearance = "moderator"
     access = permissions.is_accessible(
         db=db, firebase_id=firebase_id, clearance=clearance
     )
@@ -54,7 +54,7 @@ def create_team(
 def delete_team(
     team_id: int, firebase_id: str = Header(None), db: Session = Depends(get_db)
 ):
-    clearance = "admin"
+    clearance = "moderator"
     access = permissions.is_accessible(
         db=db, firebase_id=firebase_id, clearance=clearance
     )
@@ -73,7 +73,7 @@ def update_team(
     firebase_id: str = Header(None),
     db: Session = Depends(get_db),
 ):
-    clearance = "admin"
+    clearance = "moderator"
     access = permissions.is_accessible(
         db=db, firebase_id=firebase_id, clearance=clearance
     )
@@ -416,7 +416,7 @@ def link_player_to_team(
     firebase_id: str = Header(None),
     db: Session = Depends(get_db),
 ):
-    clearance = "admin"
+    clearance = "moderator"
     access = permissions.is_accessible(
         db=db, firebase_id=firebase_id, clearance=clearance
     )
@@ -454,7 +454,7 @@ def unlink_player_to_team(
     firebase_id: str = Header(None),
     db: Session = Depends(get_db),
 ):
-    clearance = "admin"
+    clearance = "moderator"
     access = permissions.is_accessible(
         db=db, firebase_id=firebase_id, clearance=clearance
     )
@@ -492,7 +492,7 @@ def set_team_captain(
     firebase_id: str = Header(None),
     db: Session = Depends(get_db),
 ):
-    clearance = "admin"
+    clearance = "moderator"
     access = permissions.is_accessible(
         db=db, firebase_id=firebase_id, clearance=clearance
     )
